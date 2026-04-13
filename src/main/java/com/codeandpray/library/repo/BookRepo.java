68a3770200813d18db8d70ab92a493c9255fdf56
samurai3452345 <magomeggaziev4@gmail.com>
2026-04-13 18:48:23 +0300

    feat: add service.BookService class, fix: BookRepo Long

--- src/main/java/com/codeandpray/library/repo/BookRepo.java
+++ src/main/java/com/codeandpray/library/repo/BookRepo.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface BookRepo extends JpaRepository<Book, Integer> {
+public interface BookRepo extends JpaRepository<Book, Long> {
 
 
     @Query("""
---
+++ src/main/java/com/codeandpray/library/service/BookService.java
@@ -0,0 +1,94 @@
+package com.codeandpray.library.service;
+
+
+import com.codeandpray.library.entity.*;
+
+import com.codeandpray.library.repo.BookRepo;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class BookService {
+
+    private final BookRepo bookRepository;
+
+    public BookService(BookRepo bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    public Page<Book> getBooks(String title, String author, String genre,
+                               String isbn, BookStatus status,
+                               int page, int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        return bookRepository.findAllByFilters(title, author, genre, isbn, status, pageable);
+    }
+
+
+    public Book getById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public Book getByTitle(String title) {
+        return bookRepository.findByTitle(title)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    public Book create(Book book) {
+
+        if (book.getTitle() == null || book.getAuthor() == null || book.getIsbn() == null) {
+            throw new RuntimeException("Invalid data");
+        }
+
+        book.setCreatedAt(LocalDateTime.now());
+        book.setUpdatedAt(LocalDateTime.now());
+        book.setStatus(BookStatus.AVAILABLE);
+
+        return bookRepository.save(book);
+    }
+
+    public Book updateById(Long id, Book updatedBook) {
+
+        Book book = getById(id);
+
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        book.setGenre(updatedBook.getGenre());
+        book.setIsbn(updatedBook.getIsbn());
+        book.setSummary(updatedBook.getSummary());
+        book.setStatus(updatedBook.getStatus());
+        book.setUpdatedAt(LocalDateTime.now());
+
+        return bookRepository.save(book);
+    }
+
+
+    public Book updateByTitle(String title, Book updatedBook) {
+
+        Book book = getByTitle(title);
+
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        book.setGenre(updatedBook.getGenre());
+        book.setIsbn(updatedBook.getIsbn());
+        book.setSummary(updatedBook.getSummary());
+        book.setStatus(updatedBook.getStatus());
+        book.setUpdatedAt(LocalDateTime.now());
+
+        return bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        Book book = getById(id);
+        bookRepository.delete(book);
+    }
+
+    public void deleteByTitle(String title) {
+        Book book = getByTitle(title);
+        bookRepository.delete(book);
+    }
+}
