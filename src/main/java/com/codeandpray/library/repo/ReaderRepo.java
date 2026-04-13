d0f25d042d85442d608ebfb023cfe8172a6798a2
Мухаммад <mukhammad.mamaevoo@mail.ru>
2026-04-13 19:55:45 +0300

    добавил 2 новых метода, получение пользователя по имени и по айди

--- src/main/java/com/codeandpray/library/repo/ReaderRepo.java
+++ src/main/java/com/codeandpray/library/repo/ReaderRepo.java
@@ -5,9 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReaderRepo extends JpaRepository<Reader, Long> {
     public List<Reader> getAllReaders();
+
     public Reader createReader(Reader reader);
+
+    public Optional<Reader> getReaderById(Long id);
+
+    public Optional<Reader> getReaderByName(String name);
 
 }
