5197b2c40d68fff64caba3696e76fe07b2b83e97
Sawaron <shoma111125@gmail.com>
2026-04-15 23:06:18 +0300

    Add Reservation entity, add resorvationRepo, resorvationService, in enums add resorvationStatus and add dto reservationResponse

--- src/main/java/com/codeandpray/library/entity/Reservation.java
+++ src/main/java/com/codeandpray/library/entity/Reservation.java
@@ -1,8 +1,41 @@
 package com.codeandpray.library.entity;
 
 
-import jakarta.persistence.Entity;
+import com.codeandpray.library.enums.ReservationStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+
+import java.time.LocalDateTime;
+
+
+@Table
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "reader_id")
+    private Reader reader;
+
+    private LocalDateTime reservationDate;
+
+    @Enumerated(EnumType.STRING)
+    private ReservationStatus status;
+
+
 }
