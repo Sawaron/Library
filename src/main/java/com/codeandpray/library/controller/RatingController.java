88a0ad4e43a09c3ec8c3a135b2247d5bd865bc97
Мухаммад <mukhammad.mamaevoo@mail.ru>
2026-04-20 10:59:17 +0300

    added in path v1

--- src/main/java/com/codeandpray/library/controller/RatingController.java
+++ src/main/java/com/codeandpray/library/controller/RatingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ratings")
+@RequestMapping("/api/v1/ratings")
 @RequiredArgsConstructor
 public class RatingController {
 
