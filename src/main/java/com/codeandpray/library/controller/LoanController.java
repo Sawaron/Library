9f9c0c912a540c6ef600ea6821e2e6e88163d8da
Sawaron <shoma111125@gmail.com>
2026-04-20 16:28:19 +0300

    Refactoring Loan after moved mapper to the directory 'mapper'

--- src/main/java/com/codeandpray/library/controller/LoanController.java
+++ src/main/java/com/codeandpray/library/controller/LoanController.java
@@ -2,14 +2,13 @@ package com.codeandpray.library.controller;
 
 import com.codeandpray.library.dto.LoanRequest;
 import com.codeandpray.library.dto.LoanResponse;
+import com.codeandpray.library.dto.PageResponse; // Твой стандарт
 import com.codeandpray.library.service.LoanService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/loans")
@@ -23,9 +22,12 @@ public class LoanController {
         return ResponseEntity.status(HttpStatus.CREATED).body(loanService.createLoan(request));
     }
 
+
     @GetMapping
-    public List<LoanResponse> getAllLoans() {
-        return loanService.getAllLoans();
+    public PageResponse<LoanResponse> getAllLoans(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return loanService.getAllLoans(page, size);
     }
 
     @GetMapping("/{id}")
@@ -38,22 +40,23 @@ public class LoanController {
         return loanService.updateLoan(id, request);
     }
 
-    // Метод возврата книги
     @PatchMapping("/{id}/return")
     public LoanResponse returnBook(@PathVariable Long id) {
         return loanService.returnBook(id);
     }
 
-    // Метод отмены (Soft Delete)
     @PatchMapping("/{id}/cancel")
     @ResponseStatus(HttpStatus.OK)
     public void cancelLoan(@PathVariable Long id) {
         loanService.cancelLoan(id);
     }
 
-    // История для конкретного пользователя
+
     @GetMapping("/reader/{readerId}")
-    public List<LoanResponse> getLoansByReader(@PathVariable Long readerId) {
-        return loanService.getLoansByReader(readerId);
+    public PageResponse<LoanResponse> getLoansByReader(
+            @PathVariable Long readerId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return loanService.getLoansByReader(readerId, page, size);
     }
 }
